edc485637616458bc95745a2616e0fa770bd437f
IT22555830 <128073104+IT22555830@users.noreply.github.com>
2023-04-01 13:24:06 +0000

    Tutorial 01 -Exercise 2

--- Tute02.c
+++ Tute02.c
@@ -11,13 +11,29 @@ e.g.
 Distance -> 20
 Amount = 20 x 50 = 1000
 
-
 Distance -> 50
 Amount = 30 x 50 + (50-30) x 40 = 2300*/
 
 #include <stdio.h>
 
-int main() {
+int main() 
+{
+  int dis;
+  float amount;
+
+  printf("\nEnter the distance(Km) the van has travelled :");
+  scanf("%d",&dis);
+
+  if(dis<=30)
+  {
+    amount=dis*50;
+  }
+  else
+  {
+    amount=(30*50) + (dis-30)*40;
+  }
+
+  printf("\nThe amount to be paid : %.2f\n",amount);
   
   return 0;
 }
